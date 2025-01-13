b77270dd496d9ea53a7b18315aa3f7dea0fa9b7a
Jeongjae <107382641+pjj11005@users.noreply.github.com>
2025-01-13 22:11:48 +0900

    feat: Create ch03 logisticregressiongd.py

---
+++ ch03/networks/logisticregressiongd.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+
+# 아달린 구현을 로지스틱 회귀 알고리즘으로 변경
+class LogisticRegressionGD:
+    """경사 하강법을 사용한 로지스틱 회귀 분류기
+
+    매개변수
+    ------------
+    eta : float
+      학습률 (0.0과 1.0 사이)
+    n_iter : int
+      훈련 데이터셋 반복 횟수
+    random_state : int
+      가중치 무작위 초기화를 위한 난수 생성기 시드
+
+    속성
+    -----------
+    w_ : 1d-array
+      학습된 가중치
+    b_ : 스칼라
+      학습된 절편 유닛
+    losses_ : list
+      각 에포크의 로지스틱 손실 함수 값
+
+    """
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """훈련 데이터 학습
+
+        매개변수
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+          n_samples 개의 샘플과 n_features 개의 특성으로 이루어진 훈련 데이터
+        y : array-like, shape = [n_samples]
+          타깃값
+
+        반환값
+        -------
+        self : object
+
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+        self.b_ = np.float_(0.)
+        self.losses_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_ += self.eta * X.T.dot(errors) / X.shape[0]
+            self.b_ += self.eta * errors.mean()
+            loss = (-y.dot(np.log(output)) - (1 - y).dot(np.log(1 - output))) / X.shape[0]
+            self.losses_.append(loss)
+        return self
+
+    def net_input(self, X):
+        """최종 입력 계산"""
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, z):
+        """로지스틱 시그모이드 활성화 계산"""
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+
+    def predict(self, X):
+        """단위 계단 함수를 사용하여 클래스 레이블을 반환합니다"""
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
